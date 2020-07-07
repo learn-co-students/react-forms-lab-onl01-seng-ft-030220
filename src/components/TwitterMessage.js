4a1cfd62f74e5b21abd7dc8c9908ad33b9442e85
Ivana <ivana_pile@yahoo.com>
2020-07-07 00:08:11 -0400

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,17 +4,25 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
   }
-
+  handleChange = event => {
+    this.setState({
+      message: event.target.value
+    })
+  }
   render() {
+    let charNumber = this.props.maxChars - this.state.message.length;
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" onChange={this.handleChange} value={this.state.message} />
+        {charNumber}
       </div>
     );
   }
 }
-
+// Its value should be saved in the component's state and should update on every change to the input.
 export default TwitterMessage;
